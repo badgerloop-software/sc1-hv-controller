aed796d2f6dc76b512f06ad37d8024c8852722ec
Wilson Guo <wigu701@gmail.com>
2024-01-28 13:19:38 -0600

    Fixed erroneous startup/shutdown outputs

--- src/digital.cpp
+++ src/digital.cpp
@@ -16,11 +16,11 @@ DigitalIn LO_CONT_TELEM(PC_10);
 DigitalIn MC_CONT_TELEM(PC_6);
 DigitalIn MPPT_CONT_TELEM(PC_8);
 DigitalIn Inertia_Fdbck(PB_2);
+DigitalIn BMS_DSRCHG_EN(PB_1);
+DigitalIn BMS_CHRG_EN(PB_15);
 
 // Outputs
 DigitalOut MCU_HV_EN(PB_7);
-DigitalOut BMS_DSRCHG_EN(PB_1);
-DigitalOut BMS_CHRG_EN(PB_15);
 DigitalOut SUPP_DEG(PC_2);
 DigitalOut DCDC_DEG(PC_3);
 DigitalOut USE_SUPP(PB_3);
@@ -29,27 +29,10 @@ DigitalOut USE_DCDC(PB_4);
 // store input digital data inside a struct to reduce the memory
 volatile struct Digital_Data digital_data = {};
 
-void read_Driver_Estop_Fdbck() {
-    digital_data.driver_EStop = Driver_Estop_Fdbck.read();
-}
-void read_Ext_Estop_Fdbck() {
-    digital_data.external_EStop = Ext_Estop_Fdbck.read();
-}
 void read_MCU_Stat_Fdbck() {
     digital_data.start_shutdown_status = MCU_Stat_Fdbck.read();
 }
-void read_Isolation_Fdbck() {
-    digital_data.isolation_status = Isolation_Fdbck.read();
-}
-void read_DSCHRG_EN_Fdbck() {
-    digital_data.battery_discharge_enabled = DSCHRG_EN_Fdbck.read();
-}
-void read_CHRG_EN_Fdbck() {
-    digital_data.battery_charge_enabled = CHRG_EN_Fdbck.read();
-}
-void read_BMS_MPO1() {
-    digital_data.BMS_MPO1 = BMS_MPO1.read();
-}
+
 void read_LO_CONT_TELEM() {
     digital_data.lv_contactor = LO_CONT_TELEM.read();
 }
@@ -58,34 +41,29 @@ void read_MC_CONT_TELEM() {
 }
 void read_MPPT_CONT_TELEM() {
     digital_data.mppt_contactor = MPPT_CONT_TELEM.read();
-}
-void read_Inertia_Fdbck() {
-    digital_data.crash_sensor = Inertia_Fdbck.read();
-}
+} 
 
 void readDigital() {
     // Startup/Shutdown
     // Read signals in same order as present on relay
-    // Turn firmware controlled signals on one by one if all previous steps are nominal (high)
-    read_Driver_Estop_Fdbck();
-    read_Ext_Estop_Fdbck();
-    read_Inertia_Fdbck();
-    if (digital_data.driver_EStop && digital_data.external_EStop && digital_data.crash_sensor) {
-        set_BMS_DSCHRG_EN(1);
-    } else {
-        set_BMS_DSCHRG_EN(0);
-    }
+    // Enable software HV EN if all signals nominal (high)
+    digital_data.driver_EStop = Driver_Estop_Fdbck.read();
+    digital_data.external_EStop = Ext_Estop_Fdbck.read();
+    digital_data.crash_sensor = Inertia_Fdbck.read();
+    digital_data.battery_discharge_enable = BMS_DSRCHG_EN.read();
+    digital_data.battery_discharge_enabled = DSCHRG_EN_Fdbck.read();
+    digital_data.battery_charge_enable = BMS_CHRG_EN.read();
+    digital_data.battery_charge_enabled = CHRG_EN_Fdbck.read();
+    digital_data.BMS_MPO1 = BMS_MPO1.read();
+    digital_data.isolation_status = Isolation_Fdbck.read();
 
-    read_DSCHRG_EN_Fdbck();
-    if (digital_data.battery_discharge_enabled) {
-        set_BMS_CHRG_EN(1);
-    } else {
-        set_BMS_CHRG_EN(0);
-    }
-
-    read_CHRG_EN_Fdbck();
-    read_Isolation_Fdbck();
-    if (digital_data.battery_charge_enabled && digital_data.isolation_status && startup_signal) {
+    if (digital_data.driver_EStop && 
+        digital_data.external_EStop && 
+        digital_data.crash_sensor && 
+        digital_data.battery_discharge_enabled && 
+        digital_data.battery_charge_enabled && 
+        digital_data.isolation_status && 
+        startup_signal) {
         set_MCU_HV_EN(1);
     } else {
         set_MCU_HV_EN(0);
@@ -93,11 +71,9 @@ void readDigital() {
 
     // Remaining signals
     read_MCU_Stat_Fdbck();
-    read_BMS_MPO1();
     read_LO_CONT_TELEM();
     read_MC_CONT_TELEM();
     read_MPPT_CONT_TELEM();
-    
 }
 
 // Set up polling of digital IO at specified rate
@@ -107,12 +83,6 @@ void initDigital(std::chrono::microseconds readSignalPeriod) {
 
 void set_MCU_HV_EN(int value) {
     MCU_HV_EN.write(value);
-}
-void set_BMS_DSCHRG_EN(int value) {
-    BMS_DSRCHG_EN.write(value);
-}
-void set_BMS_CHRG_EN(int value) {
-    BMS_CHRG_EN.write(value);
 }
 void set_SUPP_DEG(int value) {
     SUPP_DEG.write(value);
@@ -130,7 +100,9 @@ void set_USE_DCDC(int value) {
 // Display digial data for testing
 void displayDigital() {
     printf("Digital Input Signals:\n");
+    printf("BMS_CHRG_EN: %d\n", BMS_CHRG_EN.read());
     printf("CHRG_EN_Fdbck: %d\n", digital_data.battery_charge_enabled);
+    printf("BMS_DSCHRG_EN: %d\n", BMS_DSRCHG_EN.read());
     printf("DSCHRG_EN_Fdbck: %d\n", digital_data.battery_discharge_enabled);
     printf("BMS_MPO1: %d\n", digital_data.BMS_MPO1);
     printf("Inertia_Fdbck: %d\n", digital_data.crash_sensor);
@@ -144,8 +116,6 @@ void displayDigital() {
     printf("\n");
 
     printf("Digital Output Signals:\n");
-    printf("BMS_CHRG_EN: %d\n", BMS_CHRG_EN.read());
-    printf("BMS_DSCHRG_EN: %d\n", BMS_DSRCHG_EN.read());
     printf("DCDC_DEG: %d\n", DCDC_DEG.read());
     printf("SUPP_DEG: %d\n", SUPP_DEG.read());
     printf("MCU_HV_EN: %d\n", MCU_HV_EN.read());
