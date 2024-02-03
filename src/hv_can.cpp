e49d107db0f94290a19ce4a5029c7ca40f898906
Jacob Alejandro <alejandro2@wisc.edu>
2024-02-02 22:21:43 -0600

    Change CAN IDs to hex

--- src/hv_can.cpp
+++ src/hv_can.cpp
@@ -27,10 +27,10 @@ void HVCANManager::readHandler(int messageID, SharedPtr<unsigned char> data, int
 void HVCANManager::sendTelemetry() {
 
     // Digital data
-    CANManager::sendMessage(300, (void*)&digital_data, sizeof(digital_data));
+    CANManager::sendMessage(0x300, (void*)&digital_data, sizeof(digital_data));
 
     // Analog values
-    CANManager::sendMessage(301, (void*)&DCDC_current_value, sizeof(DCDC_current_value));
-    CANManager::sendMessage(302, (void*)&supplemental_current_value, sizeof(supplemental_current_value));
-    CANManager::sendMessage(303, (void*)&supplemental_voltage_value, sizeof(supplemental_voltage_value));
+    CANManager::sendMessage(0x301, (void*)&DCDC_current_value, sizeof(DCDC_current_value));
+    CANManager::sendMessage(0x302, (void*)&supplemental_current_value, sizeof(supplemental_current_value));
+    CANManager::sendMessage(0x303, (void*)&supplemental_voltage_value, sizeof(supplemental_voltage_value));
 }
